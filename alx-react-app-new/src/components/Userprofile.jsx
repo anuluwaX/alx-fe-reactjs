979659abe4fbdcee6491f77cc35dd102910d5e81
Anuoluwa <anuoluwapop3@gmail.com>
2025-07-20 05:37:25 +0200

    made correction on Userprofile

--- alx-react-app-new/src/components/Userprofile.jsx
+++ alx-react-app-new/src/components/Userprofile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function UserProfile(props) {
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px' }}>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '7px' }}>
       <h2 style={{ color: 'blue', fontSize: '1.5rem' }}>{props.name}</h2>
       <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
       <p>Bio: <span style={{ fontStyle: 'italic' }}>{props.bio}</span></p>
