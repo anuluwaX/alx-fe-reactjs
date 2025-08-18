940129d7ffbe1eb62d9dcc1ac42b479aa4796c2e
anuoluwa <anuoluwapop3@gmail.com>
2025-08-18 06:10:33 +0200

    corrected details in the App.jsx file

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import SearchBar from "./components/SearchBar";
+import UserCard from "./components/UserCard";
+
 function App() {
   return (
     <div>
-      <h1>GitHub User Search</h1>
-      <p>Application setup successful.</p>
+      <h1>GitHub User Search App</h1>
+      <SearchBar />
+      <UserCard />
     </div>
   );
 }
