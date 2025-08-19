b3f5e85170d19d24609944eae4027546e30b89aa
anuoluwa <anuoluwapop3@gmail.com>
2025-08-19 05:50:14 +0200

    Enhance Search UI with advanced filters and results display (steps 1-3)

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,14 +1,17 @@
-// src/services/githubService.js
+// services/githubService.js
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users";
+const BASE_URL = "https://api.github.com";
 
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/${username}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+export const searchUsers = async (query, location, minRepos, page = 1) => {
+  let q = query ? `${query}` : "";
+
+  if (location) q += `+location:${location}`;
+  if (minRepos) q += `+repos:>=${minRepos}`;
+
+  const response = await axios.get(`${BASE_URL}/search/users`, {
+    params: { q, page, per_page: 10 },
+  });
+
+  return response.data;
 };
-
