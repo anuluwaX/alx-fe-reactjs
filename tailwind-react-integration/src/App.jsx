1b748191d9fa5b4dd515528cdbad60f3279e494a
anuoluwa <anuoluwapop3@gmail.com>
2025-08-21 05:14:20 +0200

    Added styled UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
+
 
 function App() {
   const [count, setCount] = useState(0)
