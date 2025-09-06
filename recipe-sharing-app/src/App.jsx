0a7284f07aa8c5accf3e3bb248813cc13da5a324
anuoluwa <anuoluwapop3@gmail.com>
2025-09-06 14:31:29 +0200

    Configure routes in App.jsx with react-router-dom and RecipeDetails

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div className="App" style={{ maxWidth: '600px', margin: '0 auto' }}>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div className="App" style={{ maxWidth: '600px', margin: '0 auto' }}>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
